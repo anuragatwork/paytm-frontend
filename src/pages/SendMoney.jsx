ad7245a684fbb0b9c8c1d7edf97d46d86e92a3e0
anurag <121740153+anuragatwork@users.noreply.github.com>
2024-07-20 10:48:01 +0530

    live balance display

--- src/pages/SendMoney.jsx
+++ src/pages/SendMoney.jsx
@@ -29,12 +29,16 @@ export const SendMoney = () => {
                         <h2 class="text-3xl font-bold text-center">Send Money</h2>
                         </div>
                         <div class="p-6">
-                        <div class="flex items-center space-x-4">
+                        <div class="flex items-center space-x-4 mb-3">
                             <div class="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
                             <span class="text-2xl text-white">{name[0].toUpperCase()}</span>
                             
                             </div>
-                            <h3 class="text-1xl font-semibold">your balance : {balance}</h3>
+                            <div className='flex-col'>
+                            <h3 class="text-1xl font-semibold">transfer to {name}</h3>
+                            <h3 class="text-1xl font-semibold">current balance : {balance}</h3>
+                            </div>
+                            
                         </div>
                         <div class="space-y-4">
                             <div class="space-y-2">
